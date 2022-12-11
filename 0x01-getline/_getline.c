6553c9db6da41e74d14d637f23db4e9953fe7a38
JP <alexanderborrell44@hotmail.com>
2022-12-11 16:47:23 -0400

    Hot Fix

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -9,36 +9,42 @@
 
 char *_getline(const int fd)
 {
-    char *buffer;
-    //int i;
-    //char * ph = "Hello World";
-    size_t sz;
+	char *buffer;
+	/**
+	* int i;
+	* char * ph = "Hello World";
+	*/
+	size_t sz;
 
-    buffer = malloc(sizeof(char) * (READ_SIZE + 1));
-    if (buffer == NULL) 
-    {
-        return (NULL);
-    }
+	buffer = malloc(sizeof(char) * (READ_SIZE + 1));
+	if (buffer == NULL)
+	{
+		return (NULL);
+	}
 
-    sz = read(fd, buffer, READ_SIZE);
-    if (sz > 0)
-    {
-        /*for (i = 0; i < sz || buffer[i] != '\0'; i++)
-        {
-            if (buffer[i] == '\0')
-            {
-                buffer[i + 1] = '\0';
-                return (buffer);
-            }
-        }*/
-        buffer[sz + 1] = '\0';
-        //printf("%d", READ_SIZE);
-        //printf("\n\n%c\n\n", buffer[READ_SIZE]);
-        //return NULL;
-        //printf("%ld\n", sz);
-        //printf("%d\n", buffer[0]);
-        return (buffer);
-    }
+	sz = read(fd, buffer, READ_SIZE);
+	if (sz > 0)
+	{
+		/**
+		* for (i = 0; i < sz || buffer[i] != '\0'; i++)
+		* {
+		*	if (buffer[i] == '\0')
+		*	{
+		*		buffer[i + 1] = '\0';
+		*		return (buffer);
+		*	}
+		* }
+		*/
+		buffer[sz + 1] = '\0';
+		/**
+		* printf("%d", READ_SIZE);
+		* printf("\n\n%c\n\n", buffer[READ_SIZE]);
+		* return NULL;
+		* printf("%ld\n", sz);
+		* Sprintf("%d\n", buffer[0]);
+		*/
+		return (buffer);
+	}
 
-    return NULL;
+	return (NULL);
 }
