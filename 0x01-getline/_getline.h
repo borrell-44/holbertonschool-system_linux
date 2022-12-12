7730b0384c21326b808271750177f2c48a874c55
JP <alexanderborrell44@hotmail.com>
2022-12-12 14:08:36 -0400

    Hot Fix

--- 0x01-getline/_getline.h
+++ 0x01-getline/_getline.h
@@ -9,22 +9,6 @@
 
 #define READ_SIZE 11
 
-/**
- * struct list_c - singly linked list
- * @id: id of the car
- * @laps: number of laps a car has done
- * @next: points to the next node
- *
- * Description: singly linked of cars laps
- */
-
-/*typedef struct list_c
-{
-    int id;
-    int laps;
-    struct list_c *next;
-};*/
-
 char *_getline(const int fd);
 
 #endif
